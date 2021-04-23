b6c4ae80a6cc79b6e07403611f22c3d84884ef2d
taehyunKim <office.div1201@gmail.com>
2021-04-23 16:52:09 +0900

    Modify: API 주소 변경

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ export const STAR_API = 'http://13.125.224.154:8000/ratings?user=1&product=1';
 export const LOGIN_API = 'http://13.125.224.154:8000/users/signin';
 export const CART_API = 'http://13.125.224.154:8000/orders/cart';
 export const MENU_API = 'http://13.125.224.154:8000/products/menu-bar';
-export const PRDDETAIL_API = 'http://13.125.224.154:8000/products';
+export const PRDDETAIL_API = 'http://13.125.224.154:8000//products';
 export const SIGNUP_API = 'http://13.125.224.154:8000/users/signup';
 export const MAIN_API = 'http://13.125.224.154:8000/products/main';
